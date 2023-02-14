747bf81c64159b7076f8b5554a3b85bc100b7af6
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-02-14 18:18:47 +0100

    Dotrine Repository

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+        // dd($posts->findAll());
+        // dd($posts->find(1));
+        // dd($posts->findOneBy(['title'=>'welcome EN']));
+        // dd($posts->findBy(['title'=>'welcome EN']));
+
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('welcome from controller');
+        // $microPost->setCreated(new \DateTime);
+        // $posts->save( $microPost,true);
+        // $microPost = $posts->find(4);
+        // $microPost->setTitle('welcome from controller updated');
+        // $posts->save( $microPost,true);
+
+        $microPost = $posts->find(4);
+         $posts->remove( $microPost,true);
+
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
